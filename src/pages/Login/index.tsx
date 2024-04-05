682a88878d2794a0d8c538378a081a57b66fc9b2
Minseok Choe <choems@outlook.com>
2024-04-05 16:19:34 +0900

    Edit Login Page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import axios from 'axios';
-import { Box, Button, Divider, Stack, TextField, Typography } from '@mui/material';
+import { Box, Button, Stack, TextField, ToggleButton, Typography } from '@mui/material';
+
+import { useSessionId } from '../../contexts/SessionIdContext';
 
 export function LoginPage() {
   const [input, setInput] = useState({
@@ -10,6 +12,9 @@ export function LoginPage() {
   });
 
   const navigate = useNavigate();
+  const { setSessionId } = useSessionId();
+
+  const [saveLoginInfo, setSaveLoginInfo] = useState(false);
 
   function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
     setInput({
@@ -18,40 +23,35 @@ export function LoginPage() {
     });
   }
 
-  /**
-   * 회원가입 버튼을 클릭하면 발생하는 함수입니다.
-   * 백엔드 서버에 회원가입 요청을 보냅니다.
-   * 회원가입이 완료되면 메인 페이지로 이동합니다.
-   * 상태코드 201은 생성 성공을 의미합니다.
-   * navigate('주소')는 해당 주소로 이동하는 함수입니다.
-   * 참고로, navigate(-1)은 이전 페이지로 이동하는 함수입니다.
-   */
   async function handleLogin() {
     try {
-      const response = await axios.post(``, input, {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/login`, input, {
         headers: {
           'Content-Type': 'application/json',
         },
       });
 
-      if (response.status === 201) {
-        window.alert('회원가입이 완료되었습니다.');
+      console.log(response);
+
+      if (response.status === 200) {
+        window.alert('로그인이 완료되었습니다.');
+        setSessionId('sesssionId from backend server');
         navigate('/');
       }
     } catch (e) {
-      window.alert('회원가입에 실패했습니다.');
+      window.alert('로그인에 실패했습니다.');
     }
   }
 
   return (
     <Box padding={2} paddingTop={4}>
       <Box marginBottom={4} textAlign={'center'}>
+        <Button variant="outlined" color="primary" onClick={() => navigate(-1)}>
+          이전
+        </Button>
         <Typography variant="h4">로그인</Typography>
       </Box>
       <Box>
-        <Box marginY={2}>
-          <Divider />
-        </Box>
         <Stack spacing={2}>
           <TextField required id="id" label="ID" onChange={handleInput} />
           <TextField required id="firstName" type="password" label="비밀번호" onChange={handleInput} />
@@ -59,11 +59,21 @@ export function LoginPage() {
       </Box>
       <Box paddingY={6}>
         <Stack spacing={3} direction="row" justifyContent={'center'}>
-          <Button variant="outlined" color="primary" onClick={() => navigate(-1)}>
-            이전
+          <ToggleButton value="check" selected={saveLoginInfo} onClick={() => setSaveLoginInfo(!saveLoginInfo)}>
+            로그인 정보 저장
+          </ToggleButton>
+          <Button variant="contained" color="primary" onClick={handleLogin}>
+            비밀번호 찾기
           </Button>
+        </Stack>
+      </Box>
+      <Box>
+        <Stack spacing={3} direction="row" justifyContent={'center'}>
           <Button variant="contained" color="primary" onClick={handleLogin}>
             로그인
+          </Button>
+          <Button variant="contained" color="primary" onClick={() => navigate('./register')}>
+            회원가입
           </Button>
         </Stack>
       </Box>
