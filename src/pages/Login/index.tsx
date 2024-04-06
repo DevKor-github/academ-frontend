8bd991731bb5c25b9c5365256f8e64d225390e31
Minseok Choe <choems@outlook.com>
2024-04-07 00:37:07 +0900

    Design 로그인 페이지가 디자인과 더 유사해지도록 수정
    * Modal로 표시하는 것은 일단 보류함

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import axios from 'axios';
-import { Box, Button, Stack, TextField, ToggleButton, Typography } from '@mui/material';
+import { Box, Button, Stack, TextField, ToggleButton, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { useSessionId } from '../../contexts/SessionIdContext';
+import { VStack } from '../../components/VStack';
+import Spacer from '../../components/Spacer';
+import { HStack } from '../../components/HStack';
+
+import styles from './index.module.css';
 
 export function LoginPage() {
   const [input, setInput] = useState({
@@ -34,7 +40,6 @@ export function LoginPage() {
       if (response.status === 200) {
         window.alert('로그인이 완료되었습니다.');
         setSessionId('sesssionId from backend server');
-
         navigate('/');
       }
     } catch (e) {
@@ -43,38 +48,39 @@ export function LoginPage() {
   }
 
   return (
-    <Box padding={2} paddingTop={4}>
-      <Box marginBottom={4} textAlign={'center'}>
-        <Button variant="outlined" color="primary" onClick={() => navigate(-1)}>
-          이전
-        </Button>
-        <Typography variant="h4">로그인</Typography>
-      </Box>
-      <Box>
-        <Stack spacing={2}>
-          <TextField required id="id" label="ID" onChange={handleInput} />
-          <TextField required id="firstName" type="password" label="비밀번호" onChange={handleInput} />
-        </Stack>
-      </Box>
-      <Box paddingY={6}>
-        <Stack spacing={3} direction="row" justifyContent={'center'}>
-          <ToggleButton value="check" selected={saveLoginInfo} onClick={() => setSaveLoginInfo(!saveLoginInfo)}>
-            로그인 정보 저장
-          </ToggleButton>
-          <Button variant="contained" color="primary" onClick={handleLogin}>
-            비밀번호 찾기
-          </Button>
-        </Stack>
-      </Box>
-      <Box>
-        <Stack spacing={3} direction="row" justifyContent={'center'}>
+    <Box className={styles.container}>
+      <Box className={styles.modal}>
+        <HStack>
+          <VStack>
+            <Spacer />
+            <IconButton onClick={() => navigate(-1)}>
+              <CloseIcon></CloseIcon>
+            </IconButton>
+          </VStack>
+
+          <HStack>
+            <TextField required id="id" label="ID" onChange={handleInput} />
+            <TextField required id="firstName" type="password" label="비밀번호" onChange={handleInput} />
+          </HStack>
+          <Stack spacing={3} direction="row" justifyContent={'center'}>
+            <ToggleButton value="check" selected={saveLoginInfo} onClick={() => setSaveLoginInfo(!saveLoginInfo)}>
+              로그인 정보 저장
+            </ToggleButton>
+            <Spacer />
+            <Button variant="contained" color="primary" onClick={handleLogin}>
+              비밀번호 찾기
+            </Button>
+          </Stack>
           <Button variant="contained" color="primary" onClick={handleLogin}>
             로그인
           </Button>
-          <Button variant="contained" color="primary" onClick={() => navigate('./register')}>
-            회원가입
-          </Button>
-        </Stack>
+          <VStack>
+            아이디가 없으신가요?
+            <Button variant="contained" color="primary" onClick={() => navigate('./register')}>
+              회원가입
+            </Button>
+          </VStack>
+        </HStack>
       </Box>
     </Box>
   );
