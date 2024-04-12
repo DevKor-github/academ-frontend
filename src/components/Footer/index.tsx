3256a6e01878ccb5c5be78eeefe4676643d2b4a8
Minseok Choe <choems@outlook.com>
2024-04-12 14:44:21 +0900

    Fix Layout of Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import VStack from '../VStack';
 import Spacer from '../Spacer';
+import A from '../A';
 
 import styles from './index.module.css';
 
@@ -7,7 +8,18 @@ export default function Footer() {
   return (
     <footer>
       <VStack className={styles.padding}>
-        <VStack>Logo Name</VStack>
+        <VStack>
+          Logo Name
+          <A style="monotone" href="/policy">
+            이용약관
+          </A>
+          <A style="monotone" href="/policy">
+            제보 및 신고
+          </A>
+          <A style="monotone" href="/policy">
+            Lorem
+          </A>
+        </VStack>
         <Spacer />
         <VStack>Copyright ⓒ 2024 Academ. all rights reserved</VStack>
       </VStack>
