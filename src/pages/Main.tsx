8dee44f2054ddc657de42bc2ce7b69935372c6f1
Minseok Choe <choems@outlook.com>
2024-04-26 03:27:03 +0900

    Refactor SearchForm 분리

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,31 +1,21 @@
-import { useState } from 'react';
-
-import { HStack, Button, VStack, Input, Typography, Spacer } from '../components';
-
-function SearchForm() {
-  const [query, setQuery] = useState('');
-
-  return (
-    <div>
-      <Input label="" value={query} onChange={(e) => setQuery(e.target.value)} />
-      <Button type="submit">검색</Button>
-    </div>
-  );
-}
+import { SearchForm } from '../components/composite';
+import { HStack, VStack, Typography } from '../components';
 
 export function MainPage() {
   return (
-    <HStack type="center" style={{ padding: '24px 0px' }}>
-      <HStack type="center" gap="5px">
-        <Typography variant="t2">
-          <b>강의명</b>으로 검색해보세요
-        </Typography>
+    <HStack gap="20px" type="center" style={{ padding: '100px 40px' }}>
+      <HStack type="center" gap="10px">
+        <VStack>
+          <Typography variant="t2" style={{ fontWeight: 'bold' }}>
+            강의명
+          </Typography>
+          <Typography variant="t2">으로 검색해보세요</Typography>
+        </VStack>
         <Typography variant="t5">e.g., Lorem Ipsum, Lorem Ipsum</Typography>
       </HStack>
       <VStack>
         <SearchForm />
       </VStack>
-      <Spacer />
     </HStack>
   );
 }
