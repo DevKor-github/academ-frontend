4babef627595e2594531c89679c4c87fe8a4d2cc
Minseok Choe <choems@outlook.com>
2024-04-26 03:24:13 +0900

    Fix Now Available to add additional style to Typography

--- src/components/base/Typography.tsx
+++ src/components/base/Typography.tsx
@@ -1,6 +1,7 @@
 interface TypographyProp {
   variant: 't1' | 't2' | 't3' | 't4' | 't5' | 't6';
   children: React.ReactNode;
+  style?: React.CSSProperties;
 }
 
 const fontSize = {
@@ -12,14 +13,14 @@ const fontSize = {
   t6: '12px',
 };
 
-export default function Typography({ variant, children }: TypographyProp) {
-  return (
-    <span
-      style={{
-        fontSize: fontSize[variant],
-      }}
-    >
-      {children}
-    </span>
-  );
+export default function Typography({ style, variant, children }: TypographyProp) {
+  const combinedStyle: React.CSSProperties = {
+    ...style,
+
+    ...{
+      fontSize: fontSize[variant],
+    },
+  };
+
+  return <span style={combinedStyle}>{children}</span>;
 }
