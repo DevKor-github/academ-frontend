70d1191dba106b78989b3c5a43602e5d7285a110
Minseok Choe <choems@outlook.com>
2024-06-18 02:49:18 +0900

    Feat Add link to diagnostic

--- components/commonLayout/footer.tsx
+++ components/commonLayout/footer.tsx
@@ -24,17 +24,19 @@ import { LogoIconRich } from "@/icons";
 export default function Footer() {
   return <footer className="pl-4 pr-4 md:pl-8 md:pr-8 border-t light:border-t-neutral-400 dark:border-t-neutral-700 light:bg-neutral-100 dark:bg-neutral-800 pt-8 pb-8">
     <AdaptiveStack vGap="20px" hGap="24px">
-        <LogoIconRich width={`${150 * 0.6}px`} height={`${39 * 0.6}px`} />
-        <span style={{ height: 'fit-content' }}>
+        <Link className="" href="/about">
+          <LogoIconRich width={`${150 * 0.6}px`} height={`${39 * 0.6}px`} />
+        </Link>
           <Link href="/policy">
             이용약관
           </Link>
-        </span>
-        <span style={{ height: 'fit-content' }}>
-          <Link href="/policy">
+          <Link href="https://forms.gle/ugJaaHWVmcL1hfBS8">
             버그리포트
-          </Link>
-      </span>
+      </Link>{
+        (process.env.NODE_ENV === "development") &&
+      <Link href="/diagnostic">
+            진단
+      </Link>}
       <span className="md:ml-auto">Copyright ⓒ 2024 Academ. all rights reserved</span>
     </AdaptiveStack>
   </footer>;
