eac0c6a52aa884e60fcf692e30477b15ec89ec2f
Minseok Choe <choems@outlook.com>
2024-06-18 02:02:59 +0900

    Add admin page (SSR)

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,25 +1,33 @@
 import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/navigation";
-import { useLayoutEffect } from "react";
+import CommonLayout from "@/components/commonLayout/commonLayout";
+import { ThemeProvider } from "@/context/ThemeContext";
+
+import '@/app/globals.css';
+
 
 export default function AdminPage({ isAdmin }: { isAdmin: boolean }) {
   const router = useRouter();
 
-  useLayoutEffect(() => {
-    if (!isAdmin) {
-      router.replace('/404');
-    }
-  }, [isAdmin]);
 
   if (!isAdmin) {
+
+    router.replace('/404');
     return null; // 로딩 상태 또는 아무것도 렌더링하지 않음
+
+
+    
   }
 
   return (
-      <div>
-        <h1>Admin Page</h1>
-        <p>Welcome, Admin!</p>
-      </div>
+    <ThemeProvider>
+    <CommonLayout>
+          <div>
+            <h1>Admin Page</h1>
+            <p>Welcome, Admin!</p>
+        </div>
+      </CommonLayout>
+      </ThemeProvider>
   );
 }
 
@@ -28,7 +36,7 @@ export async function getServerSideProps(context : GetServerSidePropsContext) {
   const cookies = req.headers.cookie || "";
 
   // 쿠키에서 관리자인지 여부를 검사하는 로직을 추가
-  const isAdmin = checkAdminFromCookies(cookies);
+  const isAdmin = await checkAdminFromCookies(cookies);
 
   // 관리자가 아닌 경우 404 상태 코드와 함께 페이지를 렌더링하지 않음
   if (!isAdmin) {
@@ -43,16 +51,33 @@ export async function getServerSideProps(context : GetServerSidePropsContext) {
 }
 
 // 쿠키에서 관리자인지 여부를 검사하는 함수 예제
-function checkAdminFromCookies(cookies : string) {
+async function checkAdminFromCookies(cookies : string) {
   if (!cookies) return false;
-  
-  const cookieArray = cookies.split(';').map(cookie => cookie.trim());
-  const adminCookie = cookieArray.find(cookie => cookie.startsWith('admin='));
 
-  if (adminCookie) {
-    const adminValue = adminCookie.split('=')[1];
-    return adminValue === 'true'; // 예시로, 'admin=true' 쿠키가 있으면 관리자라고 가정
+  try {
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/login/check-login`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': cookies // 쿠키를 헤더에 추가
+      },
+      credentials: 'include' // 쿠키를 포함한 요청을 보낼 때 필요
+    });
+
+    const data = await response.json();
+    
+    
+
+    if (data?.data?.role === "ROLE_ADMIN") {
+      return true;
+    }
+  
+    return false;
+
+  } catch {
+    return false;
   }
 
-  return false;
+  
 }
