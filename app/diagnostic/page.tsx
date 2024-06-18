952573d83abf7a2b1317acf30ffe93a9da487bee
Minseok Choe <choems@outlook.com>
2024-06-19 05:07:46 +0900

    Fix diagnostic page

--- app/diagnostic/page.tsx
+++ app/diagnostic/page.tsx
@@ -8,8 +8,8 @@ import { useState } from "react";
 export default function Page() {
   const [str, setStr] = useState('연결 시도 중..');
 
-  useEffect(() => { apiCheckOnline({}).then((a) => {setStr('연결됨, 버전: ' + a.version) }).catch((e) => setStr('실패'))}, []
+  useEffect(() => { apiCheckOnline({}).then((a) => {setStr(a.version) }).catch(() => setStr('실패'))}, []
   );
 
-  return <div className="p-10 text-xl">{`Academ Backend Server 상태 : ${str}`}</div>;
+  return <div className="p-10 text-xl">Academ Backend와의 연결 상태는 다음과 같습니다:<br />{str}</div>;
 }
