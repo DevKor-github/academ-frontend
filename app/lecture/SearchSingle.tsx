fba1eb5c74f8bb363e18492d2225a66325c66d5a
Minseok Choe <choems@outlook.com>
2024-06-20 17:20:33 +0900

    Refactor name

--- app/lecture/SearchSingle.tsx
+++ app/lecture/SearchSingle.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+"use client";
 
 import { Course } from '@/lib/models/course';
 import { StarIcon } from "@/icons";
@@ -8,10 +8,11 @@ import Link from 'next/link';
 
 import BookmarkToggleButton from '@/components/composite/bookmarkToggleButton';
 
-import styles from './SingleResearchResultView.module.css';
+import styles from './SearchSingle.module.css';
 
-export default function SingleSearchResultView({ course }: { course: Course }) {
+import LectureIcon from '@/components/composite/lectureIcon';
 
+export default function SearchSingle({ course }: { course: Course }) {
 
   return (
     <Link href={`/lecture/${course.course_id}`}>
@@ -21,10 +22,11 @@ export default function SingleSearchResultView({ course }: { course: Course }) {
           style={{ justifyContent: 'space-between', alignItems: 'flex-start', paddingBottom: '24px' }}
         >
           <VStack gap="12px">
-            <div style={{ background: 'grey', aspectRatio: 1, width: '48px', height: '48px', borderRadius: '24px' }} />
-            <HStack gap="4px">
+            <LectureIcon />
+            <HStack className='items-start text-left' gap="4px">
               <span className='text-xl' >{course.name}</span>
               <span className='text-xl' >{course.professor}</span>
+              <span className='' >{course.course_code} {course.year} {course.semester}</span>
             </HStack>
           </VStack>
           <Button
@@ -38,13 +40,9 @@ export default function SingleSearchResultView({ course }: { course: Course }) {
           </Button>
         </VStack>
         <VStack style={{ justifyContent: 'space-between' }}>
-          <VStack gap="24px">
-            <span className='text-xl'  >{course.course_code}</span>
-            <span className='text-xl'  >{course.semester}</span>
-          </VStack>
           <VStack className={styles.star} style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <StarIcon width="24px" height="24px" />
-            <span className='text-xl' >{course.avg_rating}</span>
+            <span className='text-xl flex flex-row items-center' >
+            <StarIcon />{course.avg_rating}/5({course.count_comments})</span>
           </VStack>
         </VStack>
       </HStack>
