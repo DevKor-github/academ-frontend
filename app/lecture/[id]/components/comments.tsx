6b6f986a527d8c7d8c5786855077ee9b7fa31d75
Minseok Choe <choems@outlook.com>
2024-06-21 15:00:28 +0900

    Feat mypage

--- app/lecture/[id]/components/comments.tsx
+++ app/lecture/[id]/components/comments.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/basic/button';
 import CommentView from './comment';
 import Link from 'next/link';
 
-export default function CommentsView({ course_id, comments }: { course_id: number; comments: Comment[] }) {
+export default function CommentsView({ course_id, comments }: { course_id?: number; comments: Comment[] }) {
 
   return (
     <HStack className='pl-8 pr-8 pt-24 pb-24 h-full bg-neutral-100 dark:bg-neutral-900'>
@@ -13,7 +13,7 @@ export default function CommentsView({ course_id, comments }: { course_id: numbe
 
       <VStack className="items-center justify-start gap-2">
         <span className='text-2xl'>강의평 목록</span>
-        <Link href={`/lecture/${course_id}/write`}><Button>작성하기</Button></Link>
+        {course_id !== undefined && <Link href={`/lecture/${course_id}/write`}><Button>작성하기</Button></Link>}
       </VStack>
 
       <VStack className="items-center justify-end gap-2">
