930e96d146be85f0d083e11e4cf1ec0af147965a
Minseok Choe <choems@outlook.com>
2024-06-21 18:43:34 +0900

    Add Submit Page

--- app/lecture/[id]/write/submitted.tsx
+++ app/lecture/[id]/write/submitted.tsx
@@ -1,8 +1,9 @@
-export default function Submitted({ success }: { success: boolean }) {
-  if (success) {
-    return <div>성공했습니다</div>
-  }
-  else {
-    return <div>실패했습니다</div>
-  }
+import Button from "@/components/basic/button"
+import Link from "next/link"
+
+export default function Submitted({ success, back }: { success: boolean, back: string }) {
+    return <div className="p-8 w-full">
+      <div className="text-4xl font-bold w-full text-center">{success ? "작업에 성공했습니다!" : "실패했습니다"}</div>
+      <Link href={back}><Button>돌아가기 </Button></Link>
+    </div>
 }
