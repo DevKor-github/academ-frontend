b0fcdfa4002a010cf986e0e2184c0dd8ca4237a5
Minseok Choe <choems@outlook.com>
2024-06-21 18:33:34 +0900

    Design Add Aribtrary Icon Placeholder

--- components/composite/lectureIcon.tsx
+++ components/composite/lectureIcon.tsx
@@ -1,11 +1,30 @@
-export default function LectureIcon({ code , kind = "SMALL"}: { code: string, kind? : "BIG" | "SMALL" }) {
+import Image from "next/image";
+
+export default function LectureIcon({ code, kind = "SMALL" }: { code: string, kind?: "BIG" | "SMALL" }) {
   
   (function ignore(r) { return r })(code);
   
   const scale = (kind === "BIG") ? "80px" : "48px";
+  const smallScale = (kind === "BIG") ? "12px" : "4px";
 
-  return <div style={{
-    background: 'grey', aspectRatio: 1, minWidth: scale, minHeight: scale,
-    maxWidth: scale, maxHeight: scale, borderRadius: scale
-  }} />;
+  return <div className="flex flex-row justify-center items-center overflow-clip" style={{
+    background: 'white', aspectRatio: 1, minWidth: scale, minHeight: scale, width: scale, height: scale,
+    maxWidth: scale, maxHeight: scale, borderRadius: scale, overflow: 'clip',
+  }}>
+    <Image
+      className='absolute z-10'
+      loading="eager"
+      src="/cicon/symbol_2.jpg"
+      alt='banner image'
+      width={81}
+      height={105}
+      style={{
+        padding: smallScale,
+        // borderRadius: scale,
+        height: scale,
+        width: scale,
+        objectFit: "contain"
+      }}
+    />
+  </div>;
 }
