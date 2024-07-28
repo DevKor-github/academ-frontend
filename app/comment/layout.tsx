6f1132897ec8ccc61469b01a604f34bc852f601d
Minseok Choe <choems@outlook.com>
2024-07-28 23:46:04 +0900

    Add Comment View Layout

---
+++ app/comment/layout.tsx
@@ -0,0 +1,9 @@
+import CommonLayout from '@/components/commonLayout/commonLayout';
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <CommonLayout>{children}</CommonLayout>;
+}
