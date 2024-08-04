5fc4279ffa7fdc0093802dad5442e2c355decde9
Minseok Choe <choems@outlook.com>
2024-08-05 00:35:53 +0900

    Fix MyPage

--- app/mypage/client.tsx
+++ app/mypage/client.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useSessionId } from '../../context/SessionIdContext';
 import CommentsView from '../lecture/[id]/components/comments';
 
@@ -11,54 +12,11 @@ import { HStack, VStack } from '@/components/basic/stack';
 import { isBookmark } from '@/lib/api/course';
 import Link from 'next/link';
 
+import ManageMembership from './inner/ManageMembership';
+import UserDataOverview from './inner/UserdataOverview';
+
 function NoSessionIdFallback() {
   return <div>이 기능을 사용하려면 로그인해야 합니다.</div>;
-}
-
-function UserData({ userprofile }: { userprofile: UserProfile }) {
-  return (
-    <div className="p-8 text-xl col-auto">
-      <div className="text-3xl pb-4">내 정보</div>
-      <Link href="/mypage/edit">수정하기</Link>
-      <div>
-        <span className="font-bold">이름</span> {userprofile.username}
-      </div>
-      <div>
-        <span className="font-bold">학번</span> {userprofile.student_id}
-      </div>
-      <div>
-        <span className="font-bold">과정</span> {userprofile.degree}
-      </div>
-      <div>
-        <span className="font-bold">학과</span> {userprofile.department}
-      </div>
-      <div>
-        <span className="font-bold">포인트</span> {userprofile.point}
-      </div>
-    </div>
-  );
-}
-
-function ManageMembership({ profile }: { profile: UserProfile }) {
-  let expr = new Date(profile.access_expiration_date);
-
-  return (
-    <div>
-      <div>
-        <div>
-          강의평 작성 시 100 포인트, 작성한 강의평이 좋아요를 10개 이상 받을 시 50 포인트를 얻을 수 있습니다. 강의평
-          삭제 시 100 포인트가 차감됩니다. 신규가입자는 한 달 동안 자유롭게 열람이 가능하며, 이후 열람하기 위해서는
-          강의평을 작성하여 포인트를 받아야합니다
-        </div>
-        {expr.toLocaleDateString()}
-      </div>
-      <div>
-        <div>30일</div>
-        <div>90일</div>
-        <div>180일</div>
-      </div>
-    </div>
-  );
 }
 
 function CoursesView({ courses }: { courses: (Course & isBookmark)[] }) {
@@ -98,7 +56,7 @@ export default function MyPage() {
   if (myprofile.status === 'SUCCESS') {
     return (
       <main className="w-full flex-grow">
-        <UserData userprofile={myprofile.data} />
+        <UserDataOverview userprofile={myprofile.data} />
         <ManageMembership profile={myprofile.data} />
         <CoursesView courses={myprofile.data.courses} />
         <CommentsView comments={myprofile.data.comments} />
