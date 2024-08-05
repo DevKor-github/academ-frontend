45e9c1e210ca2a9a37af04229bbabe9e13349d1c
qpaqpleqp <qpaqpleqp@korea.ac.kr>
2024-08-05 20:53:32 +0900

    Fix notice with API

---
+++ app/notice/fetch.tsx
@@ -0,0 +1,76 @@
+import { useSessionId } from '@/context/SessionIdContext';
+import { apiNoticeList } from '@/lib/api/notice';
+import { useEffect, useState } from 'react';
+import NoticeSingle from './NoticeSingle';
+import { HStack } from '@/components/basic/stack';
+import { Notice } from '@/lib/api/notice';
+import Button from '@/components/basic/button';
+import { DownIcon } from '@/icons';
+
+function NoticeListView({
+  notices,
+  setPage,
+  eon,
+}: {
+  notices: Notice[];
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+  eon: boolean;
+}) {
+  return (
+    <div>
+      <HStack gap="20px">
+        <div>
+          {notices.map((notice) => (
+            <NoticeSingle key={notice.notice_id} notice={notice} />
+          ))}
+        </div>
+        {eon || (
+          <div className="w-full pt-6 flex flex-col justify-center items-center">
+            <Button
+              onClick={() =>
+                setPage((v) => {
+                  return v + 1;
+                })
+              }
+            >
+              <DownIcon />
+            </Button>
+          </div>
+        )}
+      </HStack>
+    </div>
+  );
+}
+
+export default function NoticeResultsView() {
+  const [jwt] = useSessionId();
+  const [notices, setNotices] = useState<null | Notice[]>(null);
+  const [page, setPage] = useState(1);
+  const [EON, setEON] = useState<boolean>(false);
+
+  useEffect(() => {
+    apiNoticeList({ page: page }, { token: jwt?.accessToken }).then((a) => {
+      if (a.status === 'SUCCESS') {
+        if (a.data.length < 10) {
+          setEON(true);
+        }
+
+        if (page === 1) {
+          setNotices(a.data);
+        } else {
+          setNotices((notices || []).concat(a.data));
+        }
+      } else if (a.statusCode == 404) {
+        setNotices(notices || []);
+        setEON(true);
+      } else {
+      }
+    });
+  }, [page]);
+
+  if (notices === null) {
+    return <div>공지사항이 없습니다.</div>;
+  } else {
+    return <NoticeListView notices={notices} setPage={setPage} eon={EON} />;
+  }
+}
--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,17 +1,14 @@
+'use client';
+
 import { HStack } from '@/components/basic/stack';
-import { NoticeList } from './list';
-import NoticeSingle from './NoticeSingle';
+import NoticeResultsView from './fetch';
 
 export default function NoticePage() {
   return (
     <HStack gap="20px" style={{ margin: '40px' }}>
       <div className="my-10 text-4xl font-medium">공지사항</div>
       <div className="w-full border border-gray-300" />
-      <div>
-        {NoticeList.map((notice) => (
-          <NoticeSingle key={notice.id} notice={notice} />
-        ))}
-      </div>
+      <NoticeResultsView />
     </HStack>
   );
 }
