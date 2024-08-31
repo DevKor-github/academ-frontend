19cf2a9c7ef92708eeb4750f0743c6b1d17a079e
qpaqpleqp <qpaqpleqp@korea.ac.kr>
2024-09-01 03:54:31 +0900

    ToDo: CommentsView 등록순, 추천순 api 연결

--- app/lecture/[id]/components/comments.tsx
+++ app/lecture/[id]/components/comments.tsx
@@ -6,22 +6,22 @@ import Link from 'next/link';
 export default function CommentsView({ course_id, comments }: { course_id?: number; comments: AcdComment[] }) {
   return (
     <HStack
-      className="pl-2 pr-2 md:pl-8 md:pr-8 pt-24 pb-24 h-full transition-all
-    light:bg-light-back-1 dark:bg-dark-back-1
+      className="pl-2 pr-2 md:pl-8 md:pr-8 pt-12 pb-12 h-full transition-all
+    light:bg-light-back-1 dark:bg-dark-back-1 gap-6
     "
     >
-      <VStack className="items-center justify-between gap-2">
-        <VStack className="items-center justify-start gap-2">
+      <VStack className="items-center justify-between gap-2 mb-2">
+        <VStack className="items-center justify-start gap-10">
           <span className="text-2xl">강의평 목록</span>
           {course_id !== undefined && (
             <Link href={`/lecture/${course_id}/write`}>
-              <Button>작성하기</Button>
+              <Button> 강의평 작성하기</Button>
             </Link>
           )}
         </VStack>
 
         <VStack className="items-center justify-end gap-2">
-          <span className="glex-grow">등록순</span>
+          <span className="flex-grow">등록순</span>
           <span>추천순</span>
         </VStack>
       </VStack>
