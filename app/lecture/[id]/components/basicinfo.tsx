43ead43453b62337d6eee44b8964a39a2fbf59b4
qpaqpleqp <qpaqpleqp@korea.ac.kr>
2024-09-01 01:07:05 +0900

    Design: basicinfo match to figma

--- app/lecture/[id]/components/basicinfo.tsx
+++ app/lecture/[id]/components/basicinfo.tsx
@@ -3,16 +3,12 @@ import Tag from '@/components/basic/tag';
 import BookmarkToggleButton from '@/components/composite/bookmarkToggleButton';
 import LectureIcon from '@/components/composite/lectureIcon';
 
-// type RenderType<T, NewType> = {
-//   [P in keyof T]: T[P] extends number ? T[P] : NewType;
-// };
-
 export default function BasicInfoView({ course }: { course: CourseWithBookmark }) {
   return (
     <VStack
       className={` pl-8 pr-8  border-b-black bg-neutral-50 dark:bg-neutral-950`}
       style={{
-        paddingTop: '160px',
+        paddingTop: '100px',
         paddingBottom: '60px',
         justifyItems: 'center',
         alignItems: 'center',
@@ -21,18 +17,23 @@ export default function BasicInfoView({ course }: { course: CourseWithBookmark }
       gap="24px"
     >
       <LectureIcon kind="BIG" code={course.course_code} />
-      <HStack gap="10px">
-        <VStack gap="10px" style={{ alignItems: 'center', flexWrap: 'wrap' }}>
-          <span className="text-3xl">{course.name}</span>
+      <HStack gap="16px">
+        <VStack gap="40px" style={{ alignItems: 'center', flexWrap: 'wrap' }} className="w-fit">
+          <span className="text-3xl font-medium">{course.name}</span>
           <Tag className="bg-primary-100 text-primary-900 opacity-50">강의평 {course.count_comments}개</Tag>
           <BookmarkToggleButton defaultValue={course.isBookmark} id={course.course_id} />
         </VStack>
-        <VStack gap="20px" style={{ flexWrap: 'wrap' }}>
-          <span className="text-lg">{course.professor}</span>
-          <span className="text-lg">{course.course_code}</span>
-          <span className="text-lg">{course.year}</span>
-          <span className="text-lg">{course.semester}</span>
-          <span className="text-lg">{course.time_location}</span>
+        <VStack style={{ flexWrap: 'wrap' }} className="items-center text-base font-normal gap-10">
+          <span className="text-2xl">{course.professor}</span>
+          <div className="flex flex-row text-neutral-600 justify-between gap-6">
+            <span className="self-center">{course.course_code}</span>
+            <span className="border-r border-r-neutral-400" />
+            <span className="self-center">
+              {course.year}-{course.semester}
+            </span>
+            <span className="border-r border-r-neutral-400" />
+            <span className="whitespace-pre-line">{course.time_location}</span>
+          </div>
         </VStack>
       </HStack>
     </VStack>
