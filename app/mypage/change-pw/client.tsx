62a83659b2b2c9bd760a26577b34a430bc52dfdf
Minseok Choe <choems@outlook.com>
2024-09-01 16:02:36 +0900

    Fix: 비밀번호 변경 api 대응

--- app/mypage/change-pw/client.tsx
+++ app/mypage/change-pw/client.tsx
@@ -9,18 +9,18 @@ import { retryWithJWTRefresh } from '@/lib/api/authHelper';
 import UpdateBasicForm from './inner/form';
 
 export default function ChangePW() {
-  const [input, setInput] = useState<UpdatePWExtended>({ password: '', password_check: '' });
+  const [input, setInput] = useState<UpdatePWExtended>({ old_password: '', new_password: '', new_password_check: '' });
 
   const [submitted, setSubmitted] = useState<boolean | null>(null);
 
   const sessionIdState = useSessionId();
 
   function handleSubmit(finalInput: UpdatePWExtended) {
-    if (finalInput.password !== finalInput.password_check) {
+    if (finalInput.new_password !== finalInput.new_password_check) {
       return alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
     }
 
-    retryWithJWTRefresh(apiProfileUpdatePW, sessionIdState)({ password: finalInput.password }, {}).then((s) => {
+    retryWithJWTRefresh(apiProfileUpdatePW, sessionIdState)({ old_password: finalInput.old_password, new_password : finalInput.new_password }, {}).then((s) => {
       setSubmitted(s.status === 'SUCCESS');
     });
   }
