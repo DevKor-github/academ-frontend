3e0df6d1d3d384d36120d47672bfa8dfa243671f
Minseok Choe <choems@outlook.com>
2024-09-04 00:56:58 +0900

    Feat: add form helpers

---
+++ lib/models/form.ts
@@ -0,0 +1,13 @@
+type InputHandler = (
+  event: React.ChangeEvent<
+    HTMLInputElement | HTMLTextAreaElement extends infer E ? E : HTMLInputElement | HTMLTextAreaElement
+  >,
+) => void;
+
+interface FormProps<T> {
+  input: T;
+  handleInput?: InputHandler;
+  // setInput?: SetState<T>;
+  handleSubmit?: (e: React.FormEvent) => void;
+  submitting: boolean;
+}
