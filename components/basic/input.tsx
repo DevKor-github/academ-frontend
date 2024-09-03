27d06fdadf423056e09f2555fc228783dd9846cc
Minseok Choe <choems@outlook.com>
2024-09-04 00:31:42 +0900

    Fix: Input to be similar to input

--- components/basic/input.tsx
+++ components/basic/input.tsx
@@ -1,20 +1,10 @@
-interface InputProp extends React.InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
-  id: string;
-  type?: 'password' | 'email' | 'text' | 'password';
-  placeholder?: string;
-  value?: string;
-  name?: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  required?: boolean;
-}
+type InputProp = React.InputHTMLAttributes<HTMLInputElement>
 
-export default function Input({ label, ...rest }: InputProp) {
+export default function Input({ className, ...rest }: InputProp) {
   return (
     <span>
       <input
-        className="p-3 accent-primary-500 focus:border-primary-500 transition-all w-full rounded-lg light:bg-white border light:border-light-back-2 dark:bg-neutral-900 dark:border-dark-back-2"
-        placeholder={label}
+        className={"p-3 accent-primary-500 focus:border-primary-500 transition-all w-full rounded-lg light:bg-white border light:border-light-back-2 dark:bg-neutral-900 dark:border-dark-back-2 " + className}
         {...rest}
       />
     </span>
