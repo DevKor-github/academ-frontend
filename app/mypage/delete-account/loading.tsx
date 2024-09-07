7f348ba6d2124ed7e90fe5cd041af511a0dc978d
Minseok Choe <choems@outlook.com>
2024-09-07 17:57:44 +0900

    Refactor: let those page load immediately

---
+++ app/mypage/delete-account/loading.tsx
@@ -0,0 +1,7 @@
+import DeleteAccountForm from "./form";
+
+export default function DeleteAccountLoading() {
+  return (
+    <DeleteAccountForm input={{password : '', checked : false}} submitting={false} />
+  );
+}
