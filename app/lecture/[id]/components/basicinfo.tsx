3dfc4c92bbac5735d422f59350860d523da2cc0d
Minseok Choe <choems@outlook.com>
2024-09-07 22:34:35 +0900

    Refactor

--- app/lecture/[id]/components/basicinfo.tsx
+++ app/lecture/[id]/components/basicinfo.tsx
@@ -2,8 +2,20 @@ import { HStack, VStack } from '@/components/basic/stack';
 import Tag from '@/components/basic/tag';
 import BookmarkToggleButton from '@/components/composite/bookmarkToggleButton';
 import LectureIcon from '@/components/composite/lectureIcon';
+import Skeleton from '@/components/composite/skeleton';
 
-export default function BasicInfoView({ course }: { course: Course }) {
+function BasicInfoViewUnSafe(props: {
+  lectureIcon: React.ReactNode;
+  course_code: React.ReactNode;
+  course_id: React.ReactNode;
+  year: React.ReactNode;
+  semester: React.ReactNode;
+  time_location: React.ReactNode;
+  name: React.ReactNode;
+  count_comments: React.ReactNode;
+  bookmarkToggle: React.ReactNode;
+  professor: React.ReactNode;
+}) {
   return (
     <VStack
       className={` pl-8 pr-8  border-b-black bg-neutral-50 dark:bg-neutral-950`}
@@ -16,26 +28,58 @@ export default function BasicInfoView({ course }: { course: Course }) {
       }}
       gap="24px"
     >
-      <LectureIcon kind="BIG" code={course.course_code} />
+      <div className='text-5xl'>
+        {props.lectureIcon}
+      </div>
       <HStack gap="16px">
-        <VStack gap="40px" style={{ alignItems: 'center', flexWrap: 'wrap' }} className="w-fit">
-          <span className="text-3xl font-medium">{course.name}</span>
-          <Tag className="bg-primary-100 text-primary-900 opacity-50">강의평 {course.count_comments}개</Tag>
-          <BookmarkToggleButton defaultValue={course.isBookmark} id={course.course_id} />
+        <VStack gap="40px" style={{ alignItems: 'center', flexWrap: 'wrap' }} className="w-fit animate-fade">
+          <span className="text-3xl font-medium">{props.name}</span>
+          <Tag className="bg-primary-100 text-primary-900 opacity-50">강의평 {props.count_comments}개</Tag>
+          {props.bookmarkToggle}
         </VStack>
-        <VStack style={{ flexWrap: 'wrap' }} className="items-center text-base font-normal gap-10">
-          <span className="text-2xl">{course.professor}</span>
+        <VStack style={{ flexWrap: 'wrap' }} className="items-center text-base font-normal gap-10 animate-fade">
+          <span className="text-2xl">{props.professor}</span>
           <div className="flex flex-row text-neutral-600 justify-between gap-6">
-            <span className="self-center">{course.course_code}</span>
+            <span className="self-center">{props.course_code}</span>
             <span className="border-r border-r-neutral-400" />
             <span className="self-center">
-              {course.year}-{course.semester}
+              {props.year}-{props.semester}
             </span>
             <span className="border-r border-r-neutral-400" />
-            <span className="whitespace-pre-line">{course.time_location}</span>
+            <span className="whitespace-pre-line">{props.time_location}</span>
           </div>
         </VStack>
       </HStack>
     </VStack>
   );
+}
+
+export function BasicInfoViewLoading() {
+  return (<BasicInfoViewUnSafe
+    lectureIcon={<LectureIcon kind="BIG" code='' />}
+    year={<Skeleton placeholder='0000' />}
+    semester={<Skeleton placeholder='0R' />}
+    time_location={<Skeleton placeholder='?(?-?)' />}
+    name={<Skeleton placeholder='과목 이름' />}
+    professor={<Skeleton placeholder='교수명' />}
+    count_comments={<Skeleton placeholder='0' />}
+    course_code={<Skeleton placeholder='??????' />}
+    course_id={<Skeleton placeholder='?' />}
+    bookmarkToggle={<div />}
+  />);
+};
+
+export default function BasicInfoView({ course }: { course: CourseOnly }) {
+  return (<BasicInfoViewUnSafe
+    lectureIcon={<LectureIcon kind="BIG" code={course.course_code} />}
+    year={course.year}
+    semester={course.semester}
+    time_location={course.time_location}
+    name={course.name}
+    professor={course.professor}
+    count_comments={course.count_comments}
+    course_code={course.course_code}
+    course_id={course.course_id}
+    bookmarkToggle={<BookmarkToggleButton defaultValue={course.isBookmark} id={course.course_id} />}
+  />);
 }
---
+++ app/lecture/[id]/fetch.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { use, useState } from 'react';
+import Spinner from '@/components/basic/spinner';
+import { apiCourseDetail } from '@/lib/api/course';
+import dynamic from 'next/dynamic';
+import { useSessionId } from '@/context/SessionIdContext';
+
+// import { usePagination } from '@/lib/hooks/pagination';
+
+const ErrorTemplate = dynamic(() => import('@/lib/template'), {
+  ssr: false,
+  loading: () => (
+    <div className="w-full p-8 flex flex-row justify-center items-center text-6xl">
+      <Spinner />
+    </div>
+  ),
+});
+
+import LectureView from './CourseView';
+
+// function LectureWithOrder({ order }: { order: AcdCommentOrdering }) {
+
+//   const p = usePagination(api);
+
+//   return (
+//     <div />
+//   );
+// }
+
+
+export default function LectureFetch({ id }: { id: number }) {
+  const [jwt] = useSessionId();
+
+  // TODO : refactor to use usePagination.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [page, setPage] = useState(1);
+
+
+  // const { loading, response: course } = useApi(
+  //   apiCourseDetail,
+  //   { course_id: id, order: 'NEWEST', page },
+  //   { token: jwt?.accessToken },
+  // );
+
+  // if (loading) {
+  //   return (
+  //     <LectureLoading />
+  //   );
+  // }
+
+  const course = use(apiCourseDetail({ course_id : id, order : 'NEWEST', page }, { token: jwt?.accessToken }));
+
+  return course.status === 'SUCCESS' ? (
+    <LectureView course={course.data} />
+  ) : (
+    <ErrorTemplate title={course.statusCode.toString()} subtitle="오류" />
+  );
+}
