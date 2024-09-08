529c4ea49a8553cd98bf276d0cb6cf384e7c0d6f
Minseok Choe <choems@outlook.com>
2024-09-08 16:06:38 +0900

    Design: form 디자인 통일

--- app/mypage/change-pw/form.tsx
+++ app/mypage/change-pw/form.tsx
@@ -4,9 +4,8 @@ import Spinner from '@/components/basic/spinner';
 
 export default function ChangePWForm({ handleSubmit, input, submitting, handleInput }: FormProps<UpdatePWExtended>) {
   return (
-    <main className="pt-8 pb-8 h-full transition-all justify-center items-center flex flex-row pl-2 pr-2">
-      <form className="flex flex-col gap-8  w-full md:w-1/2" method="post" onSubmit={handleSubmit}>
-        <div className="pl-8 pr-8 font-bold text-xl">비밀번호 수정</div>
+      <form className="py-8 h-full transition-all self-center justify-center items-center pl-2 pr-2 flex flex-col gap-8 w-full md:w-1/2" method="post" onSubmit={handleSubmit}>
+        <span className="pl-8 pr-8 font-bold text-2xl">비밀번호 수정</span>
 
         <Input
           required
@@ -15,7 +14,7 @@ export default function ChangePWForm({ handleSubmit, input, submitting, handleIn
           autoComplete="current-password"
           placeholder="기존 비밀번호"
           value={input.old_password}
-          className="bg-light-back-1 dark:bg-dark-back-2 p-4  rounded-2xl"
+          className="bg-light-back-1 dark:bg-dark-back-2 p-4 rounded-2xl w-full"
           onChange={handleInput}
         />
 
@@ -26,7 +25,7 @@ export default function ChangePWForm({ handleSubmit, input, submitting, handleIn
           type="password"
           placeholder="새 비밀번호"
           value={input.new_password}
-          className="bg-light-back-1 dark:bg-dark-back-2 p-4  rounded-2xl"
+          className="bg-light-back-1 dark:bg-dark-back-2 p-4 rounded-2xl w-full"
           onChange={handleInput}
         />
 
@@ -37,16 +36,13 @@ export default function ChangePWForm({ handleSubmit, input, submitting, handleIn
           autoComplete="new-password"
           placeholder="새 비밀번호 확인"
           value={input.new_password_check}
-          className="bg-light-back-1 dark:bg-dark-back-2 p-4 rounded-2xl"
+          className="bg-light-back-1 dark:bg-dark-back-2 p-4 rounded-2xl w-full"
           onChange={handleInput}
         />
 
-        <div className="flex flex-row justify-center items-center mt-8">
-          <Button disabled={submitting} kind="filled" type="submit">
-            {submitting ? <Spinner /> : '비밀번호 변경'}
-          </Button>
-        </div>
+        <Button className='w-full' disabled={submitting || input.old_password === '' || input.new_password === '' || input.new_password_check === ''} kind="filled" type="submit">
+          {submitting ? <Spinner /> : '비밀번호 변경'}
+        </Button>
       </form>
-    </main>
   );
 }
--- app/mypage/delete-account/form.tsx
+++ app/mypage/delete-account/form.tsx
@@ -1,4 +1,5 @@
 import Input from '@/components/basic/input';
+import Button from '@/components/basic/button';
 import Spinner from '@/components/basic/spinner';
 
 export interface DeleteAccountInputExtended {
@@ -8,10 +9,11 @@ export interface DeleteAccountInputExtended {
 
 export default function DeleteAccountForm(props: FormProps<DeleteAccountInputExtended>) {
   return (
-    <form onSubmit={props.handleSubmit} className="flex flex-col justify-center items-center h-full w-full p-8">
-      <span className="text-3xl font-bold">계정을 삭제하시겠습니까?</span>
-      <br />
-      <span className="text-xl font-bold">삭제하기 전에 유의사항을 확인하세요.</span>
+<form className="py-8 h-full transition-all self-center justify-center items-center pl-2 pr-2 flex flex-col gap-8 w-full md:w-1/2 *:w-full" method="post" onSubmit={props.handleSubmit}>
+      <span className="text-2xl font-bold text-center">계정 삭제
+        <br />
+      <span className='text-lg'>삭제하기 전에 다음 유의사항을 확인하세요.</span>
+      </span>
       <div className="bg-primary-500 bg-opacity-5 p-8 rounded-xl my-8 border border-primary-500">
         <li>{'계정을 삭제하면 모든 포인트가 소멸되며, 복구할 수 없습니다.'}</li>
         <li>
@@ -39,14 +41,14 @@ export default function DeleteAccountForm(props: FormProps<DeleteAccountInputExt
           />
           예, 계정을 삭제하면 삭제된 포인트를 다시 복구할 수 없으며 접근 권한을 잃어버림을 이해했습니다.
         </label>
-        <button
+        <Button
           disabled={
             props.handleInput === undefined || props.input.password === '' || !props.input.checked || props.submitting
           }
           type="submit"
         >
           {props.submitting ? <Spinner /> : '탈퇴하기'}
-        </button>
+        </Button>
       </div>
     </form>
   );
