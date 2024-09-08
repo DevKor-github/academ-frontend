97bd754690748f8560f47918a0eab0cfd15ac717
Minseok Choe <choems@outlook.com>
2024-09-08 22:48:15 +0900

    Edit: fix tab count after bug fix

--- lib/directive.ts
+++ lib/directive.ts
@@ -2,4 +2,4 @@ export const isDebug = process.env.NODE_ENV === 'development';
 export const backendBaseUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 export const elemPerPage = 10;
 export const keyForUserAuth = 'userSessionId';
-export const keyToCountTabs = 'tabCount';
+export const keyToCountTabs = 'tabCountNew'; // 'tabCount' is deprecated
