4654fd11e418d674e4a5853d6f9cf3335a77f5e0
Minseok Choe <choems@outlook.com>
2024-09-17 22:58:23 +0900

    Fix type

--- lib/api/errors.ts
+++ lib/api/errors.ts
@@ -1,29 +1,29 @@
 export class AcdApiError extends Error {
-  constructor(msg: String = '') {
+  constructor(msg: string = '') {
     super(`AcademError: ${msg}`);
   }
 }
 
 export class FailedResponseError extends AcdApiError {
-  constructor(msg: String = '') {
+  constructor(msg: string = '') {
     super(`FailedResponseError: ${msg}`);
   }
 }
 
 export class NoPermissionError extends AcdApiError {
-  constructor(msg: String = '') {
+  constructor(msg: string = '') {
     super(`NoPermissionError: ${msg}`);
   }
 }
 
 export class NoResponseError extends AcdApiError {
-  constructor(msg: String = '') {
+  constructor(msg: string = '') {
     super(`NoResponseError: ${msg}`);
   }
 }
 
 export class NoRequestError extends AcdApiError {
-  constructor(msg: String = '') {
+  constructor(msg: string = '') {
     super(`NoRequestError: ${msg}`);
   }
 }
