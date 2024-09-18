338ffc66bab984fd47840b51c636236dea8ae8ff
Minseok Choe <choems@outlook.com>
2024-09-18 23:27:17 +0900

    Fix diagnostic page

--- app/diagnostic/page.tsx
+++ app/diagnostic/page.tsx
@@ -1,10 +1,25 @@
-import DiagnosticClient from './client';
+'use client';
 
-export default function DiagnosticPage() {
+import { apiCheckOnline } from '@/lib/api/calls/admin';
+import { useEffect, useState } from 'react';
+import { APP_VERSION } from '@/lib/directive';
+
+export default function DiagnosticClient() {
+  const [str, setStr] = useState('연결 시도 중..');
+
+  useEffect(() => {
+    apiCheckOnline({})
+      .then((a) => {
+        setStr(a.version);
+      })
+      .catch(() => setStr('실패'));
+  }, []);
+
   return (
-    <div className="p-10 text-xl">
-      Academ Frontend Version: {process.env.APP_VERSION}
-      <DiagnosticClient />
+    <div>
+      Academ Backend와의 연결 상태는 다음과 같습니다:
+      <br />
+      {str}
     </div>
   );
 }
