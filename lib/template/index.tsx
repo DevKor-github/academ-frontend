9c1e5f1014ec37c0eba83aad916c82dbeab5d743
Minseok Choe <choems@outlook.com>
2024-09-18 23:24:34 +0900

    Remove <main />

--- lib/template/index.tsx
+++ lib/template/index.tsx
@@ -14,27 +14,25 @@ export default function ErrorTemplate({
   back?: string;
 }) {
   return (
-    <main>
-      <HStack className="pt-24 pb-24 pl-8 pr-8 text-center items-center justify-center">
-        <ErrorIllust />
-        <HStack className="pb-16" gap="16px">
-          <span className="text-6xl font-bold">{title}</span>
-          <span className="text-xl whitespace-pre-line text-balance">{subtitle}</span>
-        </HStack>
-        <VStack className="pb-8 justify-center" gap="16px">
-          <BackButton />
-          <Link href="/">
-            <Button>메인 페이지로</Button>
-          </Link>
-        </VStack>
-        <span className="text-xs text-base-20 ">
-          이 오류가 버그인 것 같다면{' '}
-          <Link className="text-primary-500 underline" href={process.env.NEXT_PUBLIC_BUG_REPORT || ''}>
-            버그리포트
-          </Link>
-          를 부탁드려요.
-        </span>
+    <HStack className="pt-24 pb-24 pl-8 pr-8 text-center items-center justify-center">
+      <ErrorIllust />
+      <HStack className="pb-16" gap="16px">
+        <span className="text-6xl font-bold">{title}</span>
+        <span className="text-xl whitespace-pre-line text-balance">{subtitle}</span>
       </HStack>
-    </main>
+      <VStack className="pb-8 justify-center" gap="16px">
+        <BackButton />
+        <Link href="/">
+          <Button>메인 페이지로</Button>
+        </Link>
+      </VStack>
+      <span className="text-xs text-base-20 ">
+        이 오류가 버그인 것 같다면{' '}
+        <Link className="text-primary-500 underline" href={process.env.NEXT_PUBLIC_BUG_REPORT || ''}>
+          버그리포트
+        </Link>
+        를 부탁드려요.
+      </span>
+    </HStack>
   );
 }
