734cea54182efcdf84b3381ae1ca99d33b155ae4
Minseok Choe <choems@outlook.com>
2024-09-23 02:29:10 +0900

    Fix mypage

---
+++ app/mypage/part/MyBookmarksView.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { useState } from 'react';
+
+import { HStack, VStack } from '@/component/basic/stack';
+
+import { apiMyPageBookmarksCount } from '@/lib/api/calls/mypage';
+import Button from '@/component/basic/button';
+import { BookmarkIcon, RightIcon } from '@/component/icon';
+import { ELEM_PER_PAGE } from '@/lib/directive';
+// import dynamic from 'next/dynamic';
+
+import { memo } from 'react';
+import CoursePreview from '@/component/view/CoursePreview';
+import { apiMyPageBookmarks } from '@/lib/api/calls/mypage';
+import { LoaderItems } from '@/app/lecture/aux';
+import { useApi } from '@/lib/hooks/api';
+
+const ApiMyPageBookmarks = memo(
+  function ApiMyPageBookmarks({ page }: ReqPaginated) {
+    const { loading, response: bms } = useApi(apiMyPageBookmarks, { page });
+
+    if (loading) {
+      return <LoaderItems />;
+    }
+
+    if (bms.status !== 'SUCCESS') {
+      return <div>오류 발생</div>;
+    }
+
+    return bms.data.flatMap((v) => <CoursePreview key={v.course_id} course={v} />);
+  },
+  (prev, next) => prev.page === next.page,
+);
+
+function Box({ children }: React.PropsWithChildren) {
+  return (
+    <HStack className="pl-2 pr-2 md:pl-8 md:pr-8 pt-24 h-full transition-all light:bg-base-31 dark:bg-base-2">
+      <VStack className="items-center justify-start gap-2">
+        <span className="text-2xl">강의 책갈피</span>
+      </VStack>
+      <div className="overflow-scroll w-full">
+        <div className="flex flex-row w-fit gap-4 py-8">{children}</div>
+      </div>
+    </HStack>
+  );
+}
+
+export default function BookmarksView() {
+  const { loading, response: totalCount } = useApi(apiMyPageBookmarksCount, {});
+  const [page, setPage] = useState<number>(1);
+
+  if (loading) {
+    return <Box>...</Box>;
+  }
+  const totalPage = Math.ceil(totalCount.data / ELEM_PER_PAGE);
+  const pages = new Array(page).fill(null).map((_, i) => i + 1);
+
+  if (totalCount.status !== 'SUCCESS') {
+    return <div>오류</div>;
+  }
+
+  if (totalCount.data <= 0) {
+    return (
+      <Box>
+        <div className="self-center bg-base-32 p-8 rounded-xl border border-base-28">
+          표시할 강의가 없습니다. 강의 페이지에서{' '}
+          <span className="text-base-10">
+            <BookmarkIcon />
+          </span>
+          을 눌러 강의를 책갈피에 저장해보세요!
+        </div>
+      </Box>
+    );
+  }
+
+  const nextButton =
+    page >= totalPage ? (
+      <div />
+    ) : (
+      <Button kind="blank" onClick={() => setPage((n) => n + 1)}>
+        <RightIcon />
+      </Button>
+    );
+
+  return (
+    <Box>
+      {pages.flatMap((v) => (
+        <ApiMyPageBookmarks key={v} page={v} />
+      ))}
+      {nextButton}
+    </Box>
+  );
+}
---
+++ app/mypage/part/MyCommentsView.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { useState, memo } from 'react';
+
+import Button from '@/component/basic/button';
+import { DownIcon } from '@/component/icon';
+
+import { HStack, VStack } from '@/component/basic/stack';
+import { MyCommentView } from '@/component/view/CommentView';
+
+import { apiMyPageCommentsCount } from '@/lib/api/calls/mypage';
+import { apiMyPageComments } from '@/lib/api/calls/mypage';
+import { useApi } from '@/lib/hooks/api';
+
+const ApiMyPageComments = memo(
+  function ApiMyPageComments({ page }: ReqPaginated) {
+    const { loading, response: cms } = useApi(apiMyPageComments, { page });
+
+    if (loading) {
+      return <div>...</div>;
+    }
+
+    if (cms.status !== 'SUCCESS') {
+      return <div>오류 발생</div>;
+    }
+
+    return cms.data.flatMap((v) => <MyCommentView key={v.course_id} comment={v} />);
+  },
+  (prev, next) => prev.page === next.page,
+);
+
+function MyCommentsWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <HStack className="pl-2 pr-2 md:pl-8 md:pr-8 pt-12 pb-12 h-full transition-all light:bg-base-31 dark:bg-base-2 gap-6">
+      <VStack className="items-center justify-between gap-2 mb-2">
+        <VStack className="items-center justify-start gap-10">
+          <span className="text-2xl">강의평 목록</span>
+        </VStack>
+      </VStack>
+      {children}
+    </HStack>
+  );
+}
+
+export default function MyCommentsView() {
+  const { loading, response: totalPageRes } = useApi(apiMyPageCommentsCount, {});
+  const [page, setPage] = useState<number>(1);
+
+  if (loading) {
+    return <div>...</div>;
+  }
+
+  if (totalPageRes.status !== 'SUCCESS') {
+    return <div>오류</div>;
+  }
+
+  const totalPage = totalPageRes.data;
+
+  const pages = new Array(page).fill(undefined).map((_, i) => i + 1);
+
+  const nextButton =
+    page >= totalPage ? (
+      <div className="self-center">모두 불러왔습니다.</div>
+    ) : (
+      <div className="w-full pt-6 flex flex-col justify-center items-center">
+        <Button onClick={() => setPage((n) => n + 1)}>
+          <DownIcon />
+        </Button>
+      </div>
+    );
+
+  if (totalPage <= 0) {
+    return <MyCommentsWrapper>등록한 강의평이 없습니다.</MyCommentsWrapper>;
+  }
+
+  return (
+    <MyCommentsWrapper>
+      {pages.flatMap((p) => (
+        <ApiMyPageComments key={p} page={p} />
+      ))}
+      {nextButton}
+    </MyCommentsWrapper>
+  );
+}
