e515b1d6b3189d2f8609f60f4f9c51d3bd2257df
Minseok Choe <choems@outlook.com>
2024-09-23 01:57:41 +0900

    Fix Dependency

--- lib/hooks/api.ts
+++ lib/hooks/api.ts
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export function useApi<Req, Res>(apiCall: ApiCall<Req, Res>, req: Req) {
   const [loading, setLoading] = useState<boolean>(true);
   const [response, setResponse] = useState<ApiResponse<Res> | null>(null);
 
-  useEffect(() => {
+  const load = useCallback(() => {
     apiCall(req).then((a) => {
       setResponse(a);
       setLoading(false);
     });
-  }, []);
+  }, [apiCall, req]);
+
+  useEffect(load, [load]);
 
   return { response, loading } as ApiState<Res>;
 }
