0584cc9a9b5662424f848f5d3be2cb89f651c6eb
Minseok Choe <choems@outlook.com>
2024-09-23 01:50:28 +0900

    Refactor

--- component/composite/bookmarkToggleButton.tsx
+++ component/composite/bookmarkToggleButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { BookmarkIcon } from '@/lib/icons';
+import { BookmarkIcon } from '@/component/icon';
 import { apiBookmark } from '@/lib/api/calls/course';
 import { useAnimationTimeout } from '@/lib/hooks/timeout';
 
