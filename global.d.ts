111dceef6aa3e9109bb2c239315c38d16759dd39
Minseok Choe <choems@outlook.com>
2024-09-27 04:22:31 +0900

    Edit

--- global.d.ts
+++ global.d.ts
@@ -4,9 +4,4 @@ declare global {
   type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
 
   type JWT = string;
-
-  type AuthTokens = {
-    accessToken: JWT | null;
-    refreshToken: JWT | null;
-  };
 }
