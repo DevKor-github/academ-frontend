c61121cdcc346df56742a9bcdd555f8bed39a2c5
Minseok Choe <choems@outlook.com>
2024-09-27 04:33:11 +0900

    Edit: admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
 import { IS_DEBUG } from "@/lib/directive";
 import dynamic from "next/dynamic";
+import { notFound } from "next/navigation";
 
 /* Note that source of this page is OK to be shown in client for non-admin
    users. but it would be better to find some better way to exclude */
@@ -7,6 +8,6 @@ const AdminPage = dynamic(() => import('./client'), { ssr: false });
 
 const AdminPageInnter = IS_DEBUG
   ? () => (<AdminPage />)
-  : () => (<div>TODO NO MSG</div>);
+  : () => { notFound() };
 
 export default AdminPageInnter;
