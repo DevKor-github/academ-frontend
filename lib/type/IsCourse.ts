c67b5dd1dd4c18b2c6e0d005d9ae4c14f980d88c
Minseok Choe <choems@outlook.com>
2024-09-27 04:21:24 +0900

    Edit: API 변경 대응

--- lib/type/IsCourse.ts
+++ lib/type/IsCourse.ts
@@ -1,4 +1,4 @@
 export function IsCourse(course: Course | CourseOnly): course is Course {
   // @ts-expect-error intended access
-  return course?.comments !== undefined;
+  return course?.comments !== undefined || course?.avg_rating !== undefined;
 }
