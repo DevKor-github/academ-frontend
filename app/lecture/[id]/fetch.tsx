bf6d071981fad46aa49e4293eab926b12b69b2ba
Minseok Choe <choems@outlook.com>
2024-09-29 18:02:29 +0900

    refactor

--- app/lecture/[id]/fetch.tsx
+++ app/lecture/[id]/fetch.tsx
@@ -19,28 +19,47 @@ import { CommentLoadingItems } from './aux';
 import { useAuthTokens } from '@/lib/context/AuthTokensContext';
 import { LectureIdPageBotLoading } from './aux';
 
-const CommentsResults = memo(function CommentsResults({ course_id, order, page }: ReqCourseDetail) {
-  const [{ instances }] = useAuthTokens();
-  const { loading, response } = useApi(instances.refreshFirst, apiCourseDetail, { course_id, order, page });
-
-  if (loading) {
-    return <CommentLoadingItems />;
-  }
-
-  if (response.status !== 'SUCCESS') {
-    return <div>발생해서 오류가 발생했습니다.</div>;
-  }
-
-  if (!IsCourse(response.data)) {
-    return <NoMembershipView />;
-  }
-
-  return response.data.comments.flatMap((comment) => (
-    <div key={comment.comment_id} className="animate-fade">
-      <CommentView key={comment.comment_id} comment={comment} />
+function BlurredModal({ children, backdrop }: { children: React.ReactNode; backdrop: React.ReactNode }) {
+  return (
+    <div className="relative w-full h-full">
+      {/* backdrop-blur */}
+      <div className="absolute w-full animate-fade bg-white bg-opacity-80 z-10 justify-start pt-20 items-center gap-6 h-full">
+        <div className="sticky top-16 transform flex flex-col">
+          <div className="w-fit flex flex-col shadow-2xl self-center justify-center items-center p-4 md:p-8 m-4 md:m-8 gap-4 rounded-2xl bg-white border border-base-30">
+            {children}
+          </div>
+        </div>
+      </div>
+      <div className="relative -z-10 w-full h-full overflow-hidden">{backdrop}</div>
     </div>
-  ));
-});
+  );
+}
+
+const CommentsResults = memo(
+  function CommentsResults({ course_id, order, page }: ReqCourseDetail) {
+    const [{ instances }] = useAuthTokens();
+    const { loading, response } = useApi(instances.refreshFirst, apiCourseDetail, { course_id, order, page });
+
+    if (loading) {
+      return <CommentLoadingItems />;
+    }
+
+    if (response.status !== 'SUCCESS') {
+      return <div>발생해서 오류가 발생했습니다.</div>;
+    }
+
+    if (!IsCourse(response.data)) {
+      return <NoMembershipView />;
+    }
+
+    return response.data.comments.flatMap((comment) => (
+      <div key={comment.comment_id} className="animate-fade">
+        <CommentView key={comment.comment_id} comment={comment} />
+      </div>
+    ));
+  },
+  (p, n) => p.course_id === n.course_id && p.order === n.order && p.page === n.page,
+);
 
 export default function CommentsView({ course_id, totalPage }: ReqCourseRelated & { totalPage: number }) {
   const [{ instances }] = useAuthTokens();
@@ -81,22 +100,13 @@ export default function CommentsView({ course_id, totalPage }: ReqCourseRelated 
     );
 
   return course.data.count_comments === 0 ? (
-    <div className="relative w-full h-full overflow-hidden">
-      <div className="absolute w-full animate-fade bg-white bg-opacity-50 backdrop-blur z-10 justify-start pt-20 items-center gap-6 h-full">
-        <div className='sticky top-72 w-full flex flex-col items-center'>
-          <div className='w-fit flex flex-col justify-center items-center p-8 gap-4 rounded-2xl bg-white border border-base-30'>
-          <IssueIcon />
-          <span className="w-fulltext-center text-2xl text-center">강의평이 없습니다.</span>
-          <span className="w-fulltext-center text-base text-center text-primary-500 underline">
-            <Link href={`/lecture/${course.data.course_id}/write`}>작성하러 가기</Link>
-            </span>
-          </div>
-        </div>
-      </div>
-      <aside className="relative -z-10 w-full h-full">
-          <LectureIdPageBotLoading />
-      </aside>
-    </div>
+    <BlurredModal backdrop={<LectureIdPageBotLoading />}>
+      <IssueIcon />
+      <span className="w-fulltext-center text-2xl text-center">강의평이 없습니다.</span>
+      <span className="w-fulltext-center text-base text-center text-primary-500 underline">
+        <Link href={`/lecture/${course.data.course_id}/write`}>작성하러 가기</Link>
+      </span>
+    </BlurredModal>
   ) : IsCourse(course.data) ? (
     <>
       <CommentsSummaryView course={course.data} />
@@ -108,13 +118,8 @@ export default function CommentsView({ course_id, totalPage }: ReqCourseRelated 
       </CommentsWrapper>
     </>
   ) : (
-    <div className="relative w-full h-full">
-      <div className="absolute -z-10 w-full h-full">
-        <LectureIdPageBotLoading />
-      </div>
-      <div className="relative w-full animate-fade bg-white bg-opacity-50 backdrop-blur z-50 flex flex-col justify-center items-center gap-6 h-full min-h-[300px]">
-        <NoMembershipView />
-      </div>
-    </div>
+    <BlurredModal backdrop={<LectureIdPageBotLoading />}>
+      <NoMembershipView />
+    </BlurredModal>
   );
 }
