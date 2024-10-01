3db82c9135e0d313bc7cf7207749839b7c8de588
Minseok Choe <choems@outlook.com>
2024-10-01 22:09:52 +0900

    temp commit

--- app/lecture/page.tsx
+++ app/lecture/page.tsx
@@ -9,23 +9,19 @@ import SearchPage from './fetch';
 import { Box } from './aux';
 import { URL_BUG_REPORT } from '@/lib/directive';
 
-const SearchTopView = ({ query }: { query: string }) => {
-  return query ? (
-    <VStack className="gap-x-5 border-b light:border-b-base-27 dark:border-b-base-8 flex-wrap justify-between items-center md:px-24 px-10 py-28">
-      <span className="text-2xl" style={{ wordBreak: 'break-word' }}>
-        <span className="text-4xl">&quot;{query}&quot;</span> 강의 검색 결과
-      </span>
+function SearchTopView({ query, count }: { query: string, count: number }) {
+  return (
+    <VStack className="not-md:sticky not-md:top-16 z-40 bg-white flex-wrap justify-evenly items-center
+    gap-x-5 border-b light:border-b-base-27 dark:border-b-base-8
+    md:px-24 px-10 not-md:pt-8 md:py-28">
+
+      {query && <span className="text-2xl" style={{ wordBreak: 'break-word' }}>
+        <span className="text-4xl">&quot;{query}&quot;</span> 강의 검색 결과 {count >= 0 && <span className='text-base-20'>{count}개</span>}
+      </span>}
 
       <SearchForm className="sticky top-8 w-full md:w-[70%] md:max-w-[800px]" defaultValue={query} />
-    </VStack>
-  ) : (
-    <VStack
-      className="border-b light:border-b-base-30 dark:border-b-base-5 flex-wrap justify-center items-center"
-      style={{
-        padding: '110px 40px 110px 40px',
-      }}
-    >
-      <SearchForm className="w-full md:w-[70%] md:max-w-[800px]" defaultValue={query} />
+      
+
     </VStack>
   );
 };
@@ -41,7 +37,7 @@ export default async function SearchPageServer({
   if (q === '') {
     return (
       <div className="flex flex-col h-full">
-        <SearchTopView key={q} query={q} />
+        <SearchTopView key={q} query={q} count={-1} />
         <Box>
           <span>강의명, 교수명, 학수번호로 검색해보세요.</span>
         </Box>
@@ -50,13 +46,14 @@ export default async function SearchPageServer({
   } else if (q.length < 2) {
     return (
       <div className="flex flex-col h-full">
-        <SearchTopView key={q} query={q} />
+        <SearchTopView key={q} query={q} count={-1}/>
         <Box>
           <span>검색어는 2글자 이상이어야 합니다.</span>
         </Box>
       </div>
     );
   }
+
 
   const count = await GET<ReqSearch, number>('/api/course/search/count-result')({
     keyword: q,
@@ -66,7 +63,7 @@ export default async function SearchPageServer({
     if (count.statusCode === 404) {
       return (
         <div className="flex flex-col h-full">
-          <SearchTopView key={q} query={q} />
+          <SearchTopView key={q} query={q} count={0}/>
           <Box>
             <span>결과가 없습니다.</span>
           </Box>
@@ -75,7 +72,7 @@ export default async function SearchPageServer({
     } else {
       return (
         <div className="flex flex-col h-full">
-          <SearchTopView key={q} query={q} />
+          <SearchTopView key={q} query={q}count={-1}/>
           <Box>
             <span>
               알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요. 오류가 계속되는 경우,{' '}
@@ -87,9 +84,10 @@ export default async function SearchPageServer({
     }
   }
 
+
   return (
     <div className="flex flex-col h-full">
-      <SearchTopView key={q} query={q} />
+      <SearchTopView key={q} query={q} count={-1} />
       <SearchPage keyword={q} count={count} />
     </div>
   );
