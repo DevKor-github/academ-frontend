39d8c7b2533104765334f34a4ac23687b0e784c0
Minseok Choe <choems@outlook.com>
2024-11-26 22:21:51 +0900

    rename: move files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export const viewport: Viewport = {
 import localFont from 'next/font/local';
 
 const pretendard = localFont({
-  src: '../public/fonts/PretendardVariable.woff2',
+  src: 'fonts/PretendardVariable.woff2',
   display: 'swap',
   weight: '45 920',
   variable: '--font-pretendard',
