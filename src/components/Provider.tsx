4fca2f86a2a8fab6fa5a3293af6360a6eb9dc7d2
Minseok Choe <choems@outlook.com>
2024-11-27 02:46:01 +0900

    remove: token is now http only

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -4,10 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 
-import dynamic from 'next/dynamic';
-const AuthTokensProvider = dynamic(() => import('@/lib/context/AuthTokensContext'), { ssr: false });
-import ClearStorageDependOnTabs from '@/lib/context/ClearStorageDependOnTabs';
-import { KEY_FOR_ACCESS_TOKEN } from '@/lib/directive';
 interface Props {
   children: ReactNode;
 }
@@ -15,11 +11,5 @@ interface Props {
 export default function Provider({ children }: Props) {
   const [queryClient] = useState(() => new QueryClient());
 
-  return (
-    <ClearStorageDependOnTabs keys={[KEY_FOR_ACCESS_TOKEN]}>
-      <AuthTokensProvider>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-      </AuthTokensProvider>
-    </ClearStorageDependOnTabs>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
