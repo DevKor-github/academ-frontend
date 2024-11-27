5b2b6cdbc8beec1f1df6f0d0b3995ed18bf514f3
Minseok Choe <choems@outlook.com>
2024-11-27 23:33:03 +0900

    fix: bookmark Toggle Button

--- src/components/composite/bookmarkToggleButton.tsx
+++ src/components/composite/bookmarkToggleButton.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useEffect, useState, useActionState } from 'react';
+import { useEffect, useState, useActionState, useLayoutEffect } from 'react';
 import { BookmarkIcon } from '@/components/icon';
 import { useAnimationTimeout } from '@/lib/hooks/timeout';
-import { toggleBookmark } from '@/app/api/lecture.api';
+import { courseDetail, toggleBookmark } from '@/app/api/lecture.api';
 import Form from 'next/form';
+import { useQuery } from '@tanstack/react-query';
+import { twMerge } from 'tailwind-merge';
 
 async function toggleBookmarkAction(currentState: FormState, formData: FormData): Promise<FormState> {
   const course_id = Number(formData.get('course_id'));
@@ -25,6 +27,14 @@ export default function BookmarkToggleButton({ id, initialValue }: Props) {
   const [shake, resetShake] = useAnimationTimeout(600);
   const [state, formAction, isPending] = useActionState(toggleBookmarkAction, { result: null });
 
+  const { data } = useQuery({ queryKey: ['bookmark', id], queryFn: () => courseDetail(id) })
+  
+  useLayoutEffect(() => {
+    if (data) {
+      setB(data.data.isBookmark);
+    }
+   }, [data]);
+
   useEffect(() => {
     if (state) {
       if (state.result?.status === 'SUCCESS') {
@@ -37,11 +47,13 @@ export default function BookmarkToggleButton({ id, initialValue }: Props) {
   return (
     <Form action={formAction} disabled={isPending}>
       <span
-        className={`
-        ${b ? 'text-primary-500' : 'text-neutral-400 dark:text-neutral-600'}
-        ${isPending ? 'animate-pulse-beat' : ''}
-        ${shake ? 'animate-shake' : ''}
-        `}
+        className={twMerge(
+          'transition-colors',
+        b ? 'text-primary-500' : 'text-neutral-400 dark:text-neutral-600',
+        isPending ? 'animate-pulse-beat' : '',
+        shake ? 'animate-shake' : '',
+        // !data ? 'text-white' : '',
+        )}
       >
         <input name="course_id" value={id} className="hidden" readOnly />
         <button
