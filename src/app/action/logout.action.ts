b7070a67ec73c693e5c772eacde82541aabb457b
Minseok Choe <choems@outlook.com>
2025-01-15 01:03:40 +0900

    fix: use logout action

--- src/app/action/logout.action.ts
+++ src/app/action/logout.action.ts
@@ -1,14 +1,15 @@
+'use server';
+
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { revalidatePath } from 'next/cache';
 import { COOKIE_AUTH_TOKEN, COOKIE_REFRESH_TOKEN } from '@/data/constant';
 
-export async function GET() {
+export async function logout() {
   const cookieStore = await cookies();
   cookieStore.delete(COOKIE_AUTH_TOKEN);
   cookieStore.delete(COOKIE_REFRESH_TOKEN);
 
-  // remove all cached data
   revalidatePath('/', 'layout');
   redirect('/');
 }
