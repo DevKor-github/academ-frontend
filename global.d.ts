5ccaaa28b65cbb6663a4435ac8d92d530187b7de
Minseok Choe <choems@outlook.com>
2025-01-14 23:00:16 +0900

    fix: type

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,19 @@
 export {};
 import { EnvKey } from '@/util/env';
+import type {
+  ApiResponse as _ApiResponse,
+  ApiResponseSuccess as _ApiResponseSuccess,
+  ApiResponseError as _ApiResponseError,
+} from '@/types/dto/ApiResponse';
 
 declare global {
   type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
 
   type JWT = string;
+
+  type ApiResponse<T> = _ApiResponse<T>;
+  type ApiResponseSuccess<T> = _ApiResponseSuccess<T>;
+  type ApiResponseError = _ApiResponseError;
 
   declare namespace NodeJS {
     interface ProcessEnv extends Record<EnvKey, string> {}
