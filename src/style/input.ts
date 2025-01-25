4a611718f6c3a35b49be84e4321907f220d55132
Minseok Choe <choems@outlook.com>
2025-01-26 00:05:59 +0900

    feat: style of input

---
+++ src/style/input.ts
@@ -0,0 +1,16 @@
+import { tv } from 'tailwind-variants';
+
+export const input = tv({
+  base: [
+    'p-3',
+    'accent-primary-500',
+    'focus:border-primary-500',
+    'transition-all',
+    'rounded-lg',
+    'light:bg-white',
+    'border',
+    'light:border-base-30',
+    'dark:bg-base-3',
+    'dark:border-base-2',
+  ],
+});
