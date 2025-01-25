02b5f224120b537897c27e377ecce9d512598676
Minseok Choe <choems@outlook.com>
2025-01-26 00:03:06 +0900

    chore: deprecate

--- src/components/basic/input.tsx
+++ src/components/basic/input.tsx
@@ -1,5 +1,8 @@
 type InputProp = React.InputHTMLAttributes<HTMLInputElement>;
 
+/**
+ * @deprecated use tailwind variant instead
+ */
 export default function Input({ className, ...rest }: InputProp) {
   return (
     <input
