d23295e4d016118fb0382d7244d29be8e6599323
qpaqpleqp <qpaqpleqp@korea.ac.kr>
2025-02-03 21:51:13 +0900

    fix: 강의평 제출하기. action으로 변경

--- src/components/template/index.tsx
+++ src/components/template/index.tsx
@@ -18,12 +18,46 @@ export default function ErrorTemplate({
       <ErrorIllust />
       <HStack className="pb-16 gap-y-4">
         {/* <span className="text-6xl font-bold">{title}</span> */}
-        <span className="text-xl whitespace-pre-line text-balance">{subtitle}</span>
+        <span className="text-2xl whitespace-pre-line text-balance">{subtitle}</span>
       </HStack>
       <VStack className="pb-8 justify-center gap-x-4">
         <BackButton />
         <Link href="/">
           <Button>메인 페이지로</Button>
+        </Link>
+      </VStack>
+      <span className="text-xs text-base-20 ">
+        이 오류가 버그인 것 같다면{' '}
+        <Link className="text-primary-500 underline" href={process.env.NEXT_PUBLIC_BUG_REPORT || ''}>
+          버그리포트
+        </Link>
+        를 부탁드려요.
+      </span>
+    </HStack>
+  );
+}
+
+export function ErrorLogintemplate({
+  title,
+  subtitle,
+  // back
+}: {
+  title: string;
+  subtitle: string;
+  back?: string;
+}) {
+  return (
+    <HStack className="pt-24 pb-24 pl-8 pr-8 text-center items-center justify-center">
+      <ErrorIllust />
+      <HStack className="pb-16 gap-y-4">
+        {/* <span className="text-6xl font-bold">{title}</span> */}
+        <span className="text-2xl whitespace-pre-line text-balance">{subtitle}</span>
+        <span className="text-2xl whitespace-pre-line text-balance">로그인 후 사용해주십시오.</span>
+      </HStack>
+      <VStack className="pb-8 justify-center gap-x-4">
+        <BackButton />
+        <Link href="/login">
+          <Button>로그인하기</Button>
         </Link>
       </VStack>
       <span className="text-xs text-base-20 ">
